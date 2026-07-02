559bf1fe932420d4e16a2a5fb75101409bdd7bbe
Onezeus7849259 <anonymous784931@gmail.com>
2026-07-02 16:45:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ function validateConfig() {
     const warnings = [];
 
     // Check required fields
-    if (!config.valentineName) {
-        warnings.push("Valentine's name is not set! Using default.");
-        config.valentineName = "My Love";
-    }
 
     // Validate colors
     const isValidHex = (hex) => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex);
